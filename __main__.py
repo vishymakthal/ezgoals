8fe05a9aa63ff4984619bed973bce6f20066fe8a
Vishy Makthal <vmakthal@cisco.com>
2020-06-27 16:50:34 -0400

    WIP: seperated Event and notification functions to allow for plug and play notification functions in the future

--- __main__.py
+++
@@ -1,8 +0,0 @@
-from services import R
-import logging
-
-if __name__ == '__main__':
-    
-    logging.basicConfig(level=logging.INFO)
-    logging.info("starting process")
-    logging.info('goals to send: {}'.format(R.search_subreddit(''))) 
--- config/cnfg.py
+++
@@ -1,5 +0,0 @@
-# amount of seconds to look behind by
-LOOKBEHIND_SECONDS = 5 * 60
-
-# teams to keep an eye on 
-WATCHLIST = 'Arsenal|Manchester|Liverpool|Tottenham|Chelsea|Sheffield|Wolve|Leicester|Norwich|Brighton|Southampton' 
