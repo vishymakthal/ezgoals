7c2c1b966070da05cdf634e7ae81e77babe5ee66
Vishy Makthal <vmakthal@cisco.com>
2020-06-28 15:59:47 -0400

    combined functions again

---
+++ ezgoals_webex/src/ezgoals_webex/__main__.py
@@ -0,0 +1,44 @@
+from datetime import datetime
+import logging
+
+from ezgoals_webex.services.reddit_svc import Reddit
+from ezgoals_webex.services.webex_svc import WebexTeams
+
+from ezgoals_webex.config import creds, cnfg
+
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+if __name__ == '__main__':
+   main()
+    
+def main(event, ctx):
+    logger.info("starting EzGoals::Event process")
+
+    # grab the current time
+    now = datetime.utcnow().timestamp() - cnfg.LOOKBEHIND_SECONDS
+
+    # get a Reddit instance 
+    soccer_sub = Reddit(creds.CLIENT_ID, creds.CLIENT_SECRET, creds.USER_AGENT, now)
+    soccer_sub.set_watchlist(cnfg.WATCHLIST)
+
+    # obtain goals to publish from /r/soccer
+    goals = soccer_sub.search_subreddit('')
+    logger.info('goals to send: {}'.format(goals))
+    
+    logger.info("starting EzGoals::WebexNotification process")
+
+    # grab a WebexTeams instance
+    webex_teams = WebexTeams(creds.WEBEX_ROOMS)
+    
+    # send payload from reddit to Webex Teams channels
+    for goal in goals:
+        message = {
+            "markdown" : GOAL_MARKDOWN.format(goal['title'], goal['link'], goal['comments'])
+        }
+        resp = webex_teams.send_to_rooms(message)
+        if resp != "OK":
+            return "ERROR"
+    
+    return "OK"
---
+++ ezgoals_webex/src/ezgoals_webex/config/cnfg.py
@@ -0,0 +1,8 @@
+# amount of seconds to look behind by
+LOOKBEHIND_SECONDS = 5 * 60
+
+# teams to keep an eye on 
+WATCHLIST = 'Arsenal|Manchester|Liverpool|Tottenham|Chelsea|Sheffield|Wolve|Leicester|Norwich|Brighton|Southampton'
+
+# Markdown template for the Webex message
+GOAL_MARKDOWN = "####[TEST]{}\n| [Link]({}) | [Comments / Mirror Links](http://old.reddit.com{})"
