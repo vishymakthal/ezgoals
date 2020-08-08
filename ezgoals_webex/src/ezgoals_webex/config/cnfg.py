0a7a4412a5515f9e1181eca8a6acaf919fd75526
Vishy Makthal <vmakthal@cisco.com>
2020-08-08 19:48:42 -0400

    Updated watchlist for UCL

--- ezgoals_webex/src/ezgoals_webex/config/cnfg.py
+++ ezgoals_webex/src/ezgoals_webex/config/cnfg.py
@@ -2,7 +2,7 @@
 LOOKBEHIND_SECONDS = 5 * 60
 
 # teams to keep an eye on 
-WATCHLIST = 'Arsenal|Manchester|Liverpool|Tottenham|Chelsea|Sheffield|Wolve|Leicester|Norwich|Brighton|Southampton|Crystal|Burnley'
+WATCHLIST = 'Madrid|Manchester|Juventus|Barcelona|Napoli|Bayern|Chelsea|Atalanta'
 
 # Markdown template for the Webex message
 GOAL_MARKDOWN = "####{}\n [Link]({}) | [Comments / Mirror Links](http://old.reddit.com{})"
